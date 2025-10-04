6c24e701fcffc7c515de4665e7e42d604d4d2763
asuma <anpl.github@gmail.com>
2025-10-04 18:42:58 +0900

    refactor: standarize usecase messages

--- internal/usecase/update_monitor_test.go
+++ internal/usecase/update_monitor_test.go
@@ -111,11 +111,11 @@ func TestUpdateMonitorInteractor_Execute(t *testing.T) {
 					t.Errorf("[TestCase '%s'] unexpected error: %v", tt.name, err)
 				}
 				if !reflect.DeepEqual(result, tt.expected) {
-					t.Errorf("[TestCase '%s'] Result: '%+v' | Expected: '%+v'", tt.name, result, tt.expected)
+					t.Errorf("[TestCase '%s'] got: '%+v' | want: '%+v'", tt.name, result, tt.expected)
 				}
 			} else {
 				if !errors.Is(err, tt.expectedError) {
-					t.Errorf("[TestCase '%s'] Result error: '%v' | Expected: '%v'", tt.name, err, tt.expectedError)
+					t.Errorf("[TestCase '%s'] got error: '%v' | want: '%v'", tt.name, err, tt.expectedError)
 				}
 			}
 		})
