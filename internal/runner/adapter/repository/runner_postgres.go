88541431d765d832100d7d9d85f36b866f4a1b95
asuma <anpl.github@gmail.com>
2025-10-23 23:53:50 +0900

    feat: add runner postgres impl

--- internal/runner/adapter/repository/runner_postgres.go
+++ internal/runner/adapter/repository/runner_postgres.go
@@ -2,13 +2,13 @@ package repository
 
 import (
 	"context"
-	"database/sql"
 	"easy-go-monitor/db/sqlcgen"
 	"easy-go-monitor/internal/codes"
 	"easy-go-monitor/internal/runner/domain"
 	"errors"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -21,6 +21,10 @@ func NewRunnerPostgresRepository(pool *pgxpool.Pool) *RunnerPostgresRepository {
 }
 
 func toDomainRunner(s sqlcgen.Runner) *domain.Runner {
+	isActive := false
+	if s.IsActive != nil {
+		isActive = *s.IsActive
+	}
 	return &domain.Runner{
 		ID:             s.ID,
 		UserID:         s.UserID,
@@ -28,7 +32,7 @@ func toDomainRunner(s sqlcgen.Runner) *domain.Runner {
 		Name:           s.Name,
 		Region:         s.Region,
 		IntervalSecond: int(s.IntervalSecond),
-		IsActive:       *s.IsActive,
+		IsActive:       isActive,
 		CreatedAt:      s.CreatedAt.Time,
 		UpdatedAt:      s.UpdatedAt.Time,
 	}
@@ -53,10 +57,54 @@ func (r *RunnerPostgresRepository) Create(ctx context.Context, runner domain.Run
 func (r *RunnerPostgresRepository) FindByID(ctx context.Context, id uuid.UUID) (*domain.Runner, error) {
 	row, err := r.queries.FindRunnerByID(ctx, id)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
+		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, codes.ErrNotFound
 		}
 		return nil, err
 	}
 	return toDomainRunner(row), nil
+}
+
+func (r *RunnerPostgresRepository) FindAll(ctx context.Context, userID uuid.UUID) ([]*domain.Runner, error) {
+	rows, err := r.queries.FindAllRunners(ctx, userID)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, codes.ErrNotFound
+		}
+		return nil, err
+	}
+
+	result := make([]*domain.Runner, 0, len(rows))
+	for _, row := range rows {
+		result = append(result, toDomainRunner(row))
+	}
+	return result, nil
+}
+
+func (r *RunnerPostgresRepository) Update(ctx context.Context, runner domain.Runner) (*domain.Runner, error) {
+	row, err := r.queries.UpdateRunner(ctx, sqlcgen.UpdateRunnerParams{
+		ID:             runner.ID,
+		Name:           runner.Name,
+		Region:         runner.Region,
+		IntervalSecond: int32(runner.IntervalSecond),
+		IsActive:       &runner.IsActive,
+	})
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, codes.ErrNotFound
+		}
+		return nil, err
+	}
+	return toDomainRunner(row), nil
+}
+
+func (r *RunnerPostgresRepository) Delete(ctx context.Context, id uuid.UUID) error {
+	err := r.queries.DeleteRunner(ctx, id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return codes.ErrNotFound
+		}
+		return err
+	}
+	return nil
 }
